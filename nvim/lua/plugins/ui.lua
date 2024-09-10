7fa88251c54d3e9fd2e8c08dc3848fe050d905da
kxxhan <ofliberty2@gmail.com>
2024-09-10 13:45:23 +0900

    reset neoVim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,4 +1,5 @@
 return {
+  -- messages, cmdline and the popupmenu
   {
     "folke/noice.nvim",
     opts = function(_, opts)
@@ -9,13 +10,63 @@ return {
         },
         opts = { skip = true },
       })
+      local focused = true
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = function()
+          focused = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("FocusLost", {
+        callback = function()
+          focused = false
+        end,
+      })
+      table.insert(opts.routes, 1, {
+        filter = {
+          cond = function()
+            return not focused
+          end,
+        },
+        view = "notify_send",
+        opts = { stop = false },
+      })
+
+      opts.commands = {
+        all = {
+          -- options for the message history that you get with `:Noice`
+          view = "split",
+          opts = { enter = true, format = "details" },
+          filter = {},
+        },
+      }
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function(event)
+          vim.schedule(function()
+            require("noice.text.markdown").keys(event.buf)
+          end)
+        end,
+      })
+
       opts.presets.lsp_doc_border = true
     end,
   },
+  -- buffer line
   {
-    "rcarriga/nvim-notify",
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
     opts = {
-      timeout = 1000,
+      options = {
+        mode = "tabs",
+        -- separator_style = "slant",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
     },
   },
   -- filename
@@ -38,44 +89,58 @@ return {
           cursorline = true,
         },
         render = function(props)
-          local fullFilePath = vim.api.nvim_buf_get_name(props.buf)
-          local filename = vim.fn.fnamemodify(fullFilePath, ":t")
-          local parentDir = vim.fn.fnamemodify(fullFilePath, ":h:t")
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
           if vim.bo[props.buf].modified then
-            filename = "[+]" .. filename
+            filename = "[+] " .. filename
           end
 
           local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-          return { { icon, guifg = color }, { " " }, { parentDir }, { "/" }, { filename } }
+          return { { icon, guifg = color }, { " " }, { filename } }
         end,
       })
     end,
   },
-
-  -- bufferline
+  -- statusline(lualine)
   {
-    "akinsho/bufferline.nvim",
-    keys = {
-      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-    },
-    opts = {
-      options = {
-        mode = "tabs",
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-      },
-    },
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local LazyVim = require("lazyvim.util")
+      opts.sections.lualine_c[4] = {
+        LazyVim.lualine.pretty_path({
+          length = 0,
+          relative = "cwd",
+          modified_hl = "MatchParen",
+          directory_hl = "",
+          filename_hl = "Bold",
+          modified_sign = "",
+          readonly_icon = " 󰌾 ",
+        }),
+      }
+    end,
   },
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      -- local logo = [[
+      --   ██████╗ ███████╗██╗   ██╗ █████╗ ███████╗██╗     ██╗███████╗███████╗
+      --   ██╔══██╗██╔════╝██║   ██║██╔══██╗██╔════╝██║     ██║██╔════╝██╔════╝
+      --   ██║  ██║█████╗  ██║   ██║███████║███████╗██║     ██║█████╗  █████╗
+      --   ██║  ██║██╔══╝  ╚██╗ ██╔╝██╔══██║╚════██║██║     ██║██╔══╝  ██╔══╝
+      --   ██████╔╝███████╗ ╚████╔╝ ██║  ██║███████║███████╗██║██║     ███████╗
+      --   ╚═════╝ ╚══════╝  ╚═══╝  ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚═╝     ╚══════╝
+      -- ]]
+      local logo = [[
+██╗  ██╗███████╗██╗     ██╗      ██████╗             ██╗██╗  ██╗██╗███╗   ██╗██╗
+██║  ██║██╔════╝██║     ██║     ██╔═══██╗            ██║██║  ██║██║████╗  ██║██║
+███████║█████╗  ██║     ██║     ██║   ██║            ██║███████║██║██╔██╗ ██║██║
+██╔══██║██╔══╝  ██║     ██║     ██║   ██║       ██   ██║██╔══██║██║██║╚██╗██║╚═╝
+██║  ██║███████╗███████╗███████╗╚██████╔╝▄█╗    ╚█████╔╝██║  ██║██║██║ ╚████║██╗
+╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝ ╚═╝     ╚════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚═╝
+                                                                                ]]
 
-  -- statusline
-  -- {
-  --   "nvim-lualine/lualine.nvim",
-  --   event = "VeryLazy",
-  --   opts = {
-  --     options = {
-  --       theme = "solarized_dark",
-  --     },
-  --   },
-  -- },
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
 }
